cd7cc48453dde4cc72150b75778691bad9b2adc8
Omar Masri <16193809+omasri3@users.noreply.github.com>
2025-06-17 23:39:36 -0400

    buttons now update the display

--- calcLogic.js
+++ calcLogic.js
@@ -27,6 +27,7 @@ let firstOperator = null;
 let secondOperator = null;
 let operation = null;
 
+// operate function takes in first and second operator and runs the function given by the operation
 function operate(firstOperator, secondOperator, operation) {
     if (operation === "+") {
         return add(firstOperator,secondOperator);
@@ -38,5 +39,94 @@ function operate(firstOperator, secondOperator, operation) {
         return divide(firstOperator, secondOperator);
     } else {
         return error;
+    }
+}
+
+//Initialize the reference to the total display for the calculator
+let totalDisplay = document.querySelector("#totalDisplay")
+
+//Clear the text content of the display when hitting the clear button and reset to zero
+function clearDisplay() {
+    totalDisplay.textContent = "0";
+}
+
+//Write functions to add each number to the display when the button is clicked
+function addZeroToDisplay() {
+    if (totalDisplay.textContent === "0") {
+        totalDisplay.textContent = "0";
+    } else {
+        totalDisplay.textContent = totalDisplay.textContent + "0";
+    }
+}
+
+function addOneToDisplay() {
+    if (totalDisplay.textContent === "0") {
+        totalDisplay.textContent = "1";
+    } else {
+        totalDisplay.textContent = totalDisplay.textContent + "1";
+    }
+}
+
+function addTwoToDisplay() {
+    if (totalDisplay.textContent === "0") {
+        totalDisplay.textContent = "2";
+    } else {
+        totalDisplay.textContent = totalDisplay.textContent + "2";
+    }
+}
+
+function addThreeToDisplay() {
+    if (totalDisplay.textContent === "0") {
+        totalDisplay.textContent = "3";
+    } else {
+        totalDisplay.textContent = totalDisplay.textContent + "3";
+    }
+}
+
+function addFourToDisplay() {
+    if (totalDisplay.textContent === "0") {
+        totalDisplay.textContent = "4";
+    } else {
+        totalDisplay.textContent = totalDisplay.textContent + "4";
+    }
+}
+
+function addFiveToDisplay() {
+    if (totalDisplay.textContent === "0") {
+        totalDisplay.textContent = "5";
+    } else {
+        totalDisplay.textContent = totalDisplay.textContent + "5";
+    }
+}
+
+function addSixToDisplay() {
+    if (totalDisplay.textContent === "0") {
+        totalDisplay.textContent = "6";
+    } else {
+        totalDisplay.textContent = totalDisplay.textContent + "6";
+    }
+}
+
+function addSevenToDisplay() {
+    if (totalDisplay.textContent === "0") {
+        totalDisplay.textContent = "7";
+    } else {
+        totalDisplay.textContent = totalDisplay.textContent + "7";
+    }
+}
+
+function addEightToDisplay() {
+    if (totalDisplay.textContent === "0") {
+        totalDisplay.textContent = "8";
+    } else {
+        totalDisplay.textContent = totalDisplay.textContent + "8";
+    }
+}
+
+function addNineToDisplay() {
+    if (totalDisplay.textContent === "0") {
+        totalDisplay.textContent = "9";
+    } else {
+        totalDisplay.textContent = totalDisplay.textContent + "9";
     }
 }
